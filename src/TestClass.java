1f0dfc8a925b583dba44608a2d0e21c552dc2e27
bange1cm <bange1cm@cmich.edu>
2024-10-13 18:11:09 -0400

    Committing

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello plz work");
+		System.out.println("testing pushing from eclipse to github");
 
 	}
 
