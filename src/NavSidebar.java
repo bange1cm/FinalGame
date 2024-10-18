1ada9ac3e7e8d96abf1ed21e7ccb20ecb8fe5f26
bange1cm <bange1cm@cmich.edu>
2024-10-17 21:22:11 -0400

    Changing up website template to create individual and reusable components

---
+++ src/NavSidebar.java
@@ -0,0 +1,21 @@
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class NavSidebar extends VBox{
+	public NavSidebar() {
+		Button[] navigationButtons = new Button[4];
+        navigationButtons[0] = new Button("Home");
+        navigationButtons[1] = new Button("Products");
+        navigationButtons[2] = new Button("About");
+        navigationButtons[3] = new Button("Contact");
+        this.setSpacing(10);
+        for (int i = 0; i < navigationButtons.length; i++) {
+        	navigationButtons[i].setMaxWidth(Double.MAX_VALUE - 10);
+        	navigationButtons[i].setStyle("-fx-font: 20 arial; fx-padding:10");
+        	this.getChildren().add(navigationButtons[i]);
+        }
+        this.setStyle("-fx-background-color: indianred");
+        this.setPadding(new Insets(10));
+	}
+}
