3d52343b66984bfb23ef7d3553f0a9a19a740bea
GageLeFevre <lefev1gl@cmich.edu>
2024-11-08 14:08:52 -0500

    Update Utility.java

--- src/Utility.java
+++ src/Utility.java
@@ -4,6 +4,13 @@ public class Utility {
 	private static int playerHP;
 	private static int playerATK;
 	private static int playerDEF;
+
+	private static boolean hasRngSeed = false;
+	private static boolean hasConnection = false;
+	private static boolean hasFirewall = false;
+	private static boolean hasAIAssistant = false;
+	private static boolean hasVpn = false;
+	private static boolean hasAdBlock = false;
 	
 	public static void damage(int dmg) {
 		playerHP -= (dmg - (playerDEF/2));
@@ -19,4 +26,35 @@ public class Utility {
 		e.setHp(e.getHp() - (playerATK - (e.getDef() / 2)));
 		System.out.println("You attack");
 	}
+
+	public static void install(int id) {
+		switch(id) {
+			case 0:
+				hasRngSeed = true;
+				break;
+			case 1:
+				hasConnection = true;
+				break;
+			case 2:
+				hasFirewall = true;
+				break;
+			case 3:
+				if(!hasAIAssistant) {
+					hasAIAssistant = true;
+					playerHP *= 2;
+				}
+				break;
+			case 4:
+				if(!hasVpn) {
+					hasVpn = true;
+					playerATK *= 2;
+				} 
+				break;
+			case 5:
+				if(!hasAdBlock) {
+					hasAdBlock = true;
+					playerDEF *= 2;
+				} 
+				break;
+		}
 }
