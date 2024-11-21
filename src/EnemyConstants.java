e26856259387ba870fff4954106a7069c69ce644
calla <calla@35.32.28.72>
2024-11-21 13:28:08 -0800

    Fixed - bug images and enemy constants

---
+++ src/EnemyConstants.java
@@ -0,0 +1,12 @@
+
+
+public interface EnemyConstants {
+	public static String BUG1 = "src/Images/Bug1FixUp.png";
+	public static String BUG2 = "src/Images/Bug2FixUp.png";
+	public static String BUG3 = "src/Images/Bug3.png";
+	public static String BUG4 = "src/Images/Bug4.png";
+	public static String TROJAN_HORSE_BUG = "src/Images/TrojanHorseBug.png";
+	public static String LAG_WITCH = "src/Images/LagWitchFixUp.png";
+	public static String SAMSA = "src/Images/SamsaFixUp.png";
+	public static String TROJAN_HORSE = "src/Images/TrojanHorseVirus.png";
+}
