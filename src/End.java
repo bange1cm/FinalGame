d9f8763c4d1a7d85227a99901da44c68d8acb664
bange1cm <bange1cm@cmich.edu>
2024-11-23 16:44:58 -0500

    fixing end scene

--- src/End.java
+++ src/End.java
@@ -11,30 +11,20 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 public class End extends VBox{
+	static TextArea msg;
+	static File file;
+	
 	public End() {
 		//general title
 		Label text = new Label("End of Game");
 		text.getStyleClass().add("h1");
 		
 		//define text and image for end game message
-		TextArea msg =  new TextArea();
-		File file;
+		msg =  new TextArea();
 		
-		if(Utility.bugsDefeated == 0) {
-			msg.setText("Wow you really suck. You didn't fix anything on Unc's website.\nUnc says you're a horrible nephew. He loses all of his customers because you couldn't fix his website.");
-			msg.setStyle("-fx-text-fill: red; -fx-font: 20 arial");
-			file = new File("src/Images/uncle_angry.jpg");
-		}
-		else if (Utility.bugsDefeated > Utility.totalBugs) {
-			msg.setText("Good job I guess. You found some bugs.\nUnc wants you to know that he has lost some of his customers because you couldn't fix everything on his website.");
-			msg.setStyle("-fx-text-fill: orange; -fx-font: 20 arial;");
-			file = new File("src/Images/uncle_angry.jpg");
-		}
-		else {
-			msg.setText("Great job! You found all of the bugs and saved Unc's website.\nUnc thanks you <3");
-			msg.setStyle("-fx-text-fill: green; -fx-font: 20 arial");
-			file = new File("src/Images/uncle_happy.png");
-		}
+		msg.setText("Wow you really suck. You didn't fix anything on Unc's website.\nUnc says you're a horrible nephew. He loses all of his customers because you couldn't fix his website.");
+		msg.setStyle("-fx-text-fill: red; -fx-font: 20 arial");
+		file = new File("src/Images/uncle_angry.jpg");
 
 		msg.setWrapText(true);
 		msg.setEditable(false);
@@ -57,5 +47,23 @@ public class End extends VBox{
 		this.getChildren().addAll(text,msg, image, background);
 		this.setSpacing(20);
 		this.setPadding(new Insets(60, 100, 80, 80));
+	}
+	
+	public static void updateEnd() {
+		if(Utility.bugsDefeated == 0) {
+			msg.setText("Wow you really suck. You didn't fix anything on Unc's website.\nUnc says you're a horrible nephew. He loses all of his customers because you couldn't fix his website.");
+			msg.setStyle("-fx-text-fill: red; -fx-font: 20 arial");
+			file = new File("src/Images/uncle_angry.jpg");
+		}
+		else if (Utility.bugsDefeated < Utility.totalBugs) {
+			msg.setText("Good job I guess. You found some bugs.\nUnc wants you to know that he has lost some of his customers because you couldn't fix everything on his website.");
+			msg.setStyle("-fx-text-fill: orange; -fx-font: 20 arial;");
+			file = new File("src/Images/uncle_angry.jpg");
+		}
+		else {
+			msg.setText("Great job! You found all of the bugs and saved Unc's website.\nUnc thanks you <3");
+			msg.setStyle("-fx-text-fill: green; -fx-font: 20 arial");
+			file = new File("src/Images/uncle_happy.png");
+		}
 	}
 }
